eb410ee99d8e8dd650f2ac7ea3f81e616a525414
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-10-13 19:56:41 +0100

    further script variable and content optimisations

--- script.js
+++ script.js
@@ -25,21 +25,3 @@ rawTextInput.addEventListener('keyup', () => {
         checkBtn.classList.remove('active');
     }
 });
-
-
-
-
-
-
-/* store input*/
-
-
-/*reverse and store reverse*/
-
-/*remove capital letters and non-alpha*/
-
-/*output if is palindrome and if isn't*/
-
-/* innerHTML = "Yes," + filteredInput + " " + "is a palindrome!"
-
-    innerHTML = "No," + filteredInput + " " + "is not a palindrome. Try again!" */
